dc653611464e6688996b2cbd30c043e60f6df066
Michael Van Oudtshoorn <101michael108@gmail.com>
2026-01-26 17:07:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7014438029-85e68",
+  "appId": "1:743086600058:web:a744ddc0cfcac10b1970c3",
+  "apiKey": "AIzaSyCYM4lMyvQjhOR9-_FdnSp9it-HBC22DtU",
+  "authDomain": "studio-7014438029-85e68.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "743086600058"
+};
