5914bfb394f6b26632880c8a93ff1bab8710934e
Michael Van Oudtshoorn <101michael108@gmail.com>
2026-01-30 15:07:17 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:743086600058:web:a744ddc0cfcac10b1970c3",
   "apiKey": "AIzaSyCYM4lMyvQjhOR9-_FdnSp9it-HBC22DtU",
   "authDomain": "studio-7014438029-85e68.firebaseapp.com",
+  "storageBucket": "studio-7014438029-85e68.appspot.com",
   "measurementId": "",
   "messagingSenderId": "743086600058"
 };
